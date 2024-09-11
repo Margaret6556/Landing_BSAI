bde017ef9067f2e345c9ed2860f0ffcfe9580e96
Margaret Cezar <cezarlofellem@gmail.com>
2024-09-10 23:04:02 -0400

    added html and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="Landing page title"> </div>
+      <h1>Are You Ready to Take Control of Your Respiratory Health? </h1>
+      <h2>
+        Real-story: How BreathSync AI Can Save Me During an Asthma Attack
+      </h2>
+      <div className="story">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span class="quote">“</span>I never thought I would find myself in
+          such a terrifying situation. Last summer, I was on a hike with my
+          family when my asthma suddenly flared up. I had not used my inhaler
+          for a few days, and I did not even realize I had missed a dose. Out of
+          nowhere, I was struggling to breathe, and panic set in. But luckily, I
+          had my BreathSync AI inhaler with me. The app had already detected
+          that Iwould missed my scheduled doses and immediately sent a reminder
+          earlier that morning. As the attack worsened, BreathSync AI alerted me
+          with an emergency reminder, and with shaky hands, I used my inhaler
+          just in time. The AI even tracked my recovery in real-time and sent
+          data to my doctor, who was able to advise me on next steps within
+          minutes. Thanks to BreathSync AI, what could have been a
+          life-threatening event was managed quickly and effectively, giving me
+          peace of mind and, more importantly, my breath back.”
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
-
-export default App;
